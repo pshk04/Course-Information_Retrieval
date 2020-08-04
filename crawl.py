66e5cdbb776e1f41a1b03f31d2e4ceb1e408bf10
pshankar04 <35579576+pshankar04@users.noreply.github.com>
2020-08-04 15:12:42 -0400

    Adding new repo for Information Retrieval Course

---
+++ crawl.py
@@ -0,0 +1,15 @@
+from BeautifulSoup import BeautifulSoup
+import urllib2
+import re
+ 
+def getLinks(url):
+    html_page = urllib2.urlopen(url)
+    soup = BeautifulSoup(html_page)
+    links = []
+ 
+    for link in soup.findAll('a', attrs={'href': re.compile("^http://")}):
+        links.append(link.get('href'))
+ 
+    return links
+ 
+print( getLinks("https://arxiv.org/list/cs/1904?show=1128") )
